24cd9baf31a967ebeba46a9b9ec1d5fa52499b72
jcfdelpino <juan.delpino@kellogg.northwestern.edu>
2022-01-25 16:54:35 +0000

    hashes codealong

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -2,8 +2,32 @@
 # is the same as where this file is located and then run:
 # ruby 4-hashes.rb
 
-# Hashes are lists of *key-value pairs*
+# Hashes are lists of *key-value pairs* - use {} to create a hash
+profile = {
+    name: "JC",
+    location: {
+        city: "Evanston",
+        state: "Illinois"
+    },
+    status: "Student In ENTR - 451",
+    timeline: [
+        { status:"In this class", posted: "8:30am"},
+        {status: "Eating Tacos", posted: "7am"}
+    ]
+}
+puts profile
+
+# to access specific data
+
+puts profile [:name]
+puts profile [:status]
+#location = profile[:location]
+#puts location[:city]
+puts profile[:location][:city]
 
 # Accessing data from the hash
+puts "Latest Status"
+puts "-------------"
+puts profile[:timeline][0][:status]
 
 # More Complex Hashes
